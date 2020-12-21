0ade09a7ac0b935eef3e9f19c9c7ff037bce8974
pefector <or290703@gmail.com>
2020-12-21 22:58:20 +0200

    Save & load Staff

--- machineLearn3.py
+++ machineLearn3.py
@@ -3,9 +3,13 @@ A machine learning package with one hidden layer.
 Version 21/12/2020
 """
 # Package imports
+import json
+
 import numpy as np
 import matplotlib.pyplot as plt
-#np.random.seed(1) # set a seed so that the results are consistent
+
+
+# np.random.seed(1) # set a seed so that the results are consistent
 def layer_sizes(X, Y, h=4):
     """
     Arguments:
@@ -18,11 +22,12 @@ def layer_sizes(X, Y, h=4):
     n_y -- the size of the output layer
     """
     ### START CODE HERE ### (≈ 3 lines of code)
-    n_x = X.shape[1] # size of input layer
+    n_x = X.shape[1]  # size of input layer
     n_h = h
-    n_y = Y.shape[1] # size of output layer
+    n_y = Y.shape[1]  # size of output layer
     ### END CODE HERE ###
     return (n_x, n_h, n_y)
+
 
 def initialize_parameters(n_x, n_h, n_y):
     """
@@ -40,9 +45,9 @@ def initialize_parameters(n_x, n_h, n_y):
     """
 
     ### START CODE HERE ### (≈ 4 lines of code)
-    W1 = np.random.randn(n_h, n_x)*0.01
+    W1 = np.random.randn(n_h, n_x) * 0.01
     b1 = np.zeros((n_h, 1))
-    W2 = np.random.randn(n_y, n_h)*0.01
+    W2 = np.random.randn(n_y, n_h) * 0.01
     b2 = np.zeros((n_y, 1))
     ### END CODE HERE ###
 
@@ -58,7 +63,9 @@ def initialize_parameters(n_x, n_h, n_y):
 
     return parameters
 
+
 sig = lambda x: 1 / (1 + np.exp(-x))
+
 
 def forward_propagation(X, parameters):
     """
@@ -80,13 +87,13 @@ def forward_propagation(X, parameters):
 
     # Implement Forward Propagation to calculate A2 (probabilities)
     ### START CODE HERE ### (≈ 4 lines of code)
-    Z1 = W1@X.T
+    Z1 = W1 @ X.T
     A1 = np.tanh(Z1 + b1)
-    Z2 = W2@A1
+    Z2 = W2 @ A1
     A2 = sig(Z2 + b2)
     ### END CODE HERE ###
-#     print(A2.shape)
-#     assert(A2.shape == (Y.shape[0], X.shape[1]))
+    #     print(A2.shape)
+    #     assert(A2.shape == (Y.shape[0], X.shape[1]))
 
     cache = {"Z1": Z1,
              "A1": A1,
@@ -94,6 +101,7 @@ def forward_propagation(X, parameters):
              "A2": A2}
 
     return A2, cache
+
 
 def compute_cost(A2, Y, parameters):
     """
@@ -108,20 +116,22 @@ def compute_cost(A2, Y, parameters):
     cost --cross-entropy cost
     """
     A2 = A2.T
-    m = Y.shape[0] # number of example
+    m = Y.shape[0]  # number of example
     # Compute cost
     ### START CODE HERE ### (≈ 2 lines of code)
-    cost = -(1/m)*np.sum(Y*np.log(A2)+(1-Y)*np.log(1-A2))
-#     print(A2)
+    cost = -(1 / m) * np.sum(Y * np.log(A2) + (1 - Y) * np.log(1 - A2))
+    #     print(A2)
     ### END CODE HERE ###
-    cost = np.squeeze(cost)     # makes sure cost is the dimension we expect.
-                                # E.g., turns [[17]] into 17
+    cost = np.squeeze(cost)  # makes sure cost is the dimension we expect.
+    # E.g., turns [[17]] into 17
 
-    assert(isinstance(cost, float))
+    assert (isinstance(cost, float))
     return cost
 
+
 def TanHTag(z):
-    return 1-np.tanh(z)**2
+    return 1 - np.tanh(z) ** 2
+
 
 def backward_propagation(parameters, cache, X, Y):
     """
@@ -137,7 +147,7 @@ def backward_propagation(parameters, cache, X, Y):
     grads -- python dictionary containing your gradients with respect to different parameters
     """
     m = X.shape[0]
-#     print(X.shape)
+    #     print(X.shape)
 
     # First, retrieve W1 and W2 from the dictionary "parameters".
     ### START CODE HERE ### (≈ 2 lines of code)
@@ -150,16 +160,16 @@ def backward_propagation(parameters, cache, X, Y):
     A1 = cache['A1']
     A2 = cache['A2']
     ### END CODE HERE ###
-#     A2 = A2.T
+    #     A2 = A2.T
     # Backward propagation: calculate dW1, db1, dW2, db2.
     ### START CODE HERE ### (≈ 6 lines of code, corresponding to 6 equations on slide above)
     dZ2 = A2 - Y.T
-#     print(dZ2.shape, A1.shape)
-    dW2 = 1/m * dZ2@ A1.T
-    db2 = 1/m * np.sum(dZ2, axis=1, keepdims=True)
+    #     print(dZ2.shape, A1.shape)
+    dW2 = 1 / m * dZ2 @ A1.T
+    db2 = 1 / m * np.sum(dZ2, axis=1, keepdims=True)
     dZ1 = W2.T @ dZ2 * TanHTag(cache['Z1'])
-    dW1 = 1/m * dZ1 @ X
-    db1 = 1/m * np.sum(dZ1,axis=1,keepdims=True)
+    dW1 = 1 / m * dZ1 @ X
+    db1 = 1 / m * np.sum(dZ1, axis=1, keepdims=True)
     ### END CODE HERE ###
 
     grads = {"dW1": dW1,
@@ -169,7 +179,8 @@ def backward_propagation(parameters, cache, X, Y):
 
     return grads
 
-def update_parameters(parameters, grads, learning_rate = 0.5):
+
+def update_parameters(parameters, grads, learning_rate=0.5):
     """
     Updates parameters using the gradient accent update rule given above
 
@@ -211,10 +222,16 @@ def update_parameters(parameters, grads, learning_rate = 0.5):
 
     return parameters
 
+
 from sklearn.model_selection import *
+
+
 def split():
     pass
+
+
 split = train_test_split
+
 
 def fix_targets(Y, Number_of_types):
     """
@@ -230,16 +247,17 @@ def fix_targets(Y, Number_of_types):
         numbers_targets_og.append(n)
     return np.array(numbers_targets_og)
 
+
 def normalize(X):
     """
     Arguments:
     X -- dataset array
     The function return a normalized dataset array. use before splitting.
     """
-    return (X - X.mean(axis=0))/X.std(axis=0)
+    return (X - X.mean(axis=0)) / X.std(axis=0)
 
 
-def nn_model(X, Y, n_h, num_iterations = None, print_cost=False,alpha=0.1,graph=False,Max_last_best=2500):
+def nn_model(X, Y, n_h, num_iterations=None, print_cost=False, alpha=0.1, graph=False, Max_last_best=2500):
     """
     Arguments:
     X -- dataset of shape (2, number of examples)
@@ -258,13 +276,13 @@ def nn_model(X, Y, n_h, num_iterations = None, print_cost=False,alpha=0.1,graph=
 
     # print(X.shape, Y.shape, X_validation.shape, Y_validation.shape)
 
-    #np.random.seed(3)
+    # np.random.seed(3)
     n_x = layer_sizes(X, Y)[0]
     n_y = layer_sizes(X, Y)[2]
 
     # Initialize parameters, then retrieve W1, b1, W2, b2. Inputs: "n_x, n_h, n_y". Outputs = "W1, b1, W2, b2, parameters".
     ### START CODE HERE ### (≈ 5 lines of code)
-    parameters = initialize_parameters(n_x,n_h,n_y)
+    parameters = initialize_parameters(n_x, n_h, n_y)
     W1 = parameters["W1"]
     b1 = parameters["b1"]
     W2 = parameters["W2"]
@@ -286,7 +304,7 @@ def nn_model(X, Y, n_h, num_iterations = None, print_cost=False,alpha=0.1,graph=
         A2, cache = forward_propagation(X, parameters)
 
         # Cost function. Inputs: "A2, Y, parameters". Outputs: "cost".
-        cost = compute_cost(A2 ,Y, parameters)
+        cost = compute_cost(A2, Y, parameters)
 
         # Backpropagation. Inputs: "parameters, cache, X, Y". Outputs: "grads".
         grads = backward_propagation(parameters, cache, X, Y)
@@ -301,7 +319,7 @@ def nn_model(X, Y, n_h, num_iterations = None, print_cost=False,alpha=0.1,graph=
         # Validator shit
         A2_validation, cache_validation = forward_propagation(X_validation, parameters)
         # this_error_validation = np.sum((sgn(A2_validation).T - Y_validation)**2)
-        cost_validation = compute_cost(A2_validation ,Y_validation, parameters)
+        cost_validation = compute_cost(A2_validation, Y_validation, parameters)
         error_validation_history.append(cost_validation)
 
         last_best += 1
@@ -312,7 +330,8 @@ def nn_model(X, Y, n_h, num_iterations = None, print_cost=False,alpha=0.1,graph=
 
         # Print the cost every 1000 iterations
         if print_cost and i % 5 == 0:
-            print ("Cost after iteration %s:\tTrainer %f,\tValitation %f" %(i if i != 0 else "0000", cost, cost_validation))
+            print("Cost after iteration %s:\tTrainer %f,\tValitation %f" % (
+            i if i != 0 else "0000", cost, cost_validation))
 
         if num_iterations and i > num_iterations:
             break
@@ -324,17 +343,23 @@ def nn_model(X, Y, n_h, num_iterations = None, print_cost=False,alpha=0.1,graph=
     print(f"Min validation cost - {best_error_validation}, at {error_validation_history.index(best_error_validation)}")
 
     if graph:
-        plt.plot(error_history,'b-')
-        plt.plot(error_validation_history,'g-')
+        plt.plot(error_history, 'b-')
+        plt.plot(error_validation_history, 'g-')
         print("Green - Validation")
         print("Blue - Train")
 
     return best_NN
 
-def sgn(x):
-    return np.array(x>=0.5, dtype=int)
 
-def model_test(parameters,X,Y,do_return=False):
+def sgn(x):
+    return np.array(x >= 0.5, dtype=int)
+
+
+def maxIndex(X):
+    return np.array([i == np.max(i) for i in X], dtype=int)
+
+
+def model_test(parameters, X, Y, do_return=False, ChooseMax=True):
     """
     Arguments:
     parameters -- array of weights to test the NN with
@@ -343,24 +368,48 @@ def model_test(parameters,X,Y,do_return=False):
     do_return(optional,default is False) -- wether or not to return the success rate
     The function tests the NN's preformence for a given weights array
     """
-    results,lo = forward_propagation(X,parameters)
-    error = sgn(results).T - Y
-    success = sum(not(sum(abs(j) for j in i)) for i in error)
-    print(f"success rate: {round(100*success/len(Y),2)}%")
+    results, lo = forward_propagation(X, parameters)
+    if ChooseMax:
+        error = maxIndex(results.T) - Y
+    else:
+        error = sgn(results).T - Y
+    success = sum(not (sum(abs(j) for j in i)) for i in error)
+    print(f"success rate: {round(100 * success / len(Y), 2)}%")
     print(f"successfuly guessed {success} / {len(Y)} examples correctly")
     if do_return:
-        return 100*success/len(Y)
+        return 100 * success / len(Y)
 
-def apply_on_example(arr,parameters):
+
+def apply_on_example(arr, parameters):
     """
     Arguments:
     arr -- an array with data of one example
     parameters -- array of weights to test the NN on
     The function returns its guess given a weights array
     """
-    results,lo = forward_propagation(arr,parameters)
-    l=list(results)
+    results, lo = forward_propagation(arr, parameters)
+    l = list(results)
     print(f"the nn's guess: {l.index(max(results))}")
     print('\n\nhere are the guesses the nn was most confident about(marked with <----):')
     for i in range(len(l)):
         print(f"{i} {'<----' if sgn(results)[i] else ''}")
+
+
+def SaveDict(parameters, name="NN"):
+    dictlist = {}
+    for key in parameters:
+        dictlist[key] = [[j for j in i] for i in parameters[key]]
+
+    with open(name+'.json', 'w') as fp:
+        json.dump(dictlist, fp)
+
+
+def ReadDict(name="NN"):
+    with open(name+'.json', 'r') as fp:
+        dictlist = json.load(fp)
+
+    parameters = {}
+    for key in dictlist:
+        parameters[key] = np.array(dictlist[key])
+
+    return parameters
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import idx2numpy
 from machineLearn3 import *
 import matplotlib.pyplot as plt
+import json
 
 # Reading
 Test_images = idx2numpy.convert_from_file('Test_images.idx3-ubyte')
@@ -14,7 +15,9 @@ Test_labels = fix_targets(Test_labels, 10)
 Test_images = np.array([i.flatten() for i in Test_images])
 # Train_images = np.array([i.flatten() for i in Train_images])
 
-parameters = nn_model(Test_images, Test_labels, 10, print_cost=True,alpha=0.01,graph=True, Max_last_best=4000)
-model_test(parameters, Test_images, Test_labels)
+parameters = nn_model(Test_images, Test_labels, 10, num_iterations=60, print_cost=True,alpha=0.01,graph=True, Max_last_best=4000)
+# model_test(parameters, Test_images, Test_labels)
 
+model_test(parameters, Test_images, Test_labels)
+SaveDict(parameters)
 plt.show()
